0ecc8d2d072307cf437740b1528b83a851b18217
CS345K <CS345K@gmail.com>
2018-05-13 19:54:04 -0400

    dadad

--- src/main/java/com/techm/app/App.java
+++ src/main/java/com/techm/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!   you" );
+        System.out.println( "Hello World!  how do you" );
     }
 }
